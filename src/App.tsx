d8149af1be07fe3cc80254e54aec54f50a012497
RebeccaMeegahapola <rebeccameegahapola@gmail.com>
2024-10-15 18:02:11 +0530

    feat: update app

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,41 @@
-import { create } from "zustand";
+import { useStore } from "@/store/store";
 import { Button } from "./components/ui/button";
+import { Card, CardContent, CardFooter, CardHeader } from "./components/ui/card";
+import { PRODUCTS_DATA } from "./lib/mockData";
+import { ChangeQtyButtons } from "./components/ChangeQtyButtons";
+import { Cart } from "./components/Cart";
 
-const useStore = create<{  //generics - define types
-  count: number;
-  inc: () => void;
-  dec: () => void;
-}>((set) => ({
-  count: 0,
-  inc: () => set((state) => ({count: state.count + 1})),
-  dec: () => set((state) => ({count: state.count - 1})),
-}));
+export default function App() {
 
-function App() {
-
-  const store = useStore();
+ const addProduct = useStore((state) => state.addProduct); //this is responsible for adding a product to the cart
+ const cartProducts = useStore((state) => state.products); //current list of products in the cart
 
   return (
-    <>
-      <Button onClick={store.inc}>+</Button>
-      <Count />
-      <Button onClick={store.dec}>-</Button>
-    </>
+    <main className="space-y-2 dark bg-background max-w-sm mx-auto mt-2 pt-10 pb-10">
+      <Cart />
+      <h1 className="text-2xl">Products:</h1>
+      <div className="space-y-2">
+        {PRODUCTS_DATA.map((product) => (
+          <Card key={product.id}>
+            <CardHeader>{product.title}</CardHeader>
+            <CardContent>{product.price}$</CardContent>
+            <CardFooter>
+              {cartProducts.find((item) => item.id === product.id) ? (
+                <ChangeQtyButtons productId={product.id}/>
+              ) : (
+                <Button onClick={() => addProduct(product)} variant='default'>
+                  Add to Cart
+                </Button>
+              )}
+            </CardFooter>
+          </Card>
+        ))}
+      </div>
+    </main>
   )
 }
 
-function Count() {
-  const store = useStore();
-  return <>{store.count}</>
-}
+// If the product is in the cart (cartProducts.find((item) => item.id === product.id)):
+// It shows the ChangeQtyButtons component, allowing the user to change the quantity of the product in the cart.
+// If the product is not in the cart, It displays a button labeled "Add to Cart" that, when clicked, adds the product to the cart by calling addProduct(product).
 
-export default App
