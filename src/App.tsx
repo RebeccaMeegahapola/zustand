11647293915981817c49d4bef9a48fd07105bbdb
RebeccaMeegahapola <rebeccameegahapola@gmail.com>
2024-10-15 11:31:39 +0530

    feat: update app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,32 @@
+import { create } from "zustand";
 import { Button } from "./components/ui/button";
 
+const useStore = create<{  //generics - define types
+  count: number;
+  inc: () => void;
+  dec: () => void;
+}>((set) => ({
+  count: 0,
+  inc: () => set((state) => ({count: state.count + 1})),
+  dec: () => set((state) => ({count: state.count - 1})),
+}));
+
 function App() {
+
+  const store = useStore();
+
   return (
     <>
-      <Button>button</Button>
+      <Button onClick={store.inc}>+</Button>
+      <Count />
+      <Button onClick={store.dec}>-</Button>
     </>
   )
+}
+
+function Count() {
+  const store = useStore();
+  return <>{store.count}</>
 }
 
 export default App
