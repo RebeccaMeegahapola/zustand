cd3495999754902fcae53b3c30850c442c020724
RebeccaMeegahapola <rebeccameegahapola@gmail.com>
2024-10-15 18:04:12 +0530

    feat: update store

--- src/store/store.ts
+++ src/store/store.ts
@@ -2,9 +2,16 @@ import { Store } from "@/types/store";
 import { immer } from "zustand/middleware/immer"; //immer is a middleware provided by Zustand
 import { createUserSlice } from "./userSlice";
 import { create } from "zustand"; //This function initializes the Zustand store.
+import { createCartSlice } from "./cart-slice";
+import { devtools, subscribeWithSelector } from "zustand/middleware";
 
 export const useStore = create<Store>()(
-    immer((...a) => ({
-        ...createUserSlice(...a),
-    }))
+    devtools(
+        subscribeWithSelector(
+            immer((...a) => ({
+                ...createUserSlice(...a),
+                ...createCartSlice(...a),
+            }))
+        ),
+    )
 );
